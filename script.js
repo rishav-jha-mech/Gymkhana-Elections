c756fbe8af69f5f9636bddc749d6dbf40661a531
rishav-jha-mech <rishav.rj74@gmail.com>
2022-10-13 11:36:18 +0530

    Accordions added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('#card-container');
-
+const manifesto = document.querySelector('#facilities');
 const LEGENDS = [
     {
         name: 'Sankhadeep Ghosh',
@@ -8,8 +8,8 @@ const LEGENDS = [
         motto: 'Dont settle for average lets achieve the best together.',
         color: '#325895',
         time: 10000,
-        facebook : 'https://www.facebook.com/100086375027463/posts/pfbid02TZesipTayjS6zzQKfxKXcpFM31YTC7ZhxtygRyU57289hwRpbruuBfw8gt9qYGmTl/',
-        instagram : 'https://www.instagram.com/p/CjahXqZJPxT/?igshid=NmNmNjAwNzg='
+        facebook: 'https://www.facebook.com/100086375027463/posts/pfbid02TZesipTayjS6zzQKfxKXcpFM31YTC7ZhxtygRyU57289hwRpbruuBfw8gt9qYGmTl/',
+        instagram: 'https://www.instagram.com/p/CjahXqZJPxT/?igshid=NmNmNjAwNzg='
     },
     {
         name: 'Satyam Shorya Jha',
@@ -55,13 +55,13 @@ const LEGENDS = [
     },
 ];
 
-LEGENDS.map((data,index) =>{
+LEGENDS.map((data, index) => {
     const card = document.createElement('div');
     card.className = 'swiper-slide'
-    card.setAttribute('data-swiper-autoplay',data.time)
+    card.setAttribute('data-swiper-autoplay', data.time)
     card.innerHTML = `
-        <div class="card">
-            <div class="card-body text-center">
+        <div class="sw-card bg-white">
+            <div class="sw-card-body text-center">
                 <div class="d-flex justify-content-center items-center">
                     <div class="img-card" style="background: ${data.color}">
                         <img class="w-100" src="${data.image}" alt="">
@@ -71,8 +71,8 @@ LEGENDS.map((data,index) =>{
                     <h6 class="fw-bold">${data.post}</h6>
                     <article class="my-3">"${data.motto}"</article>
                     <div class="d-flex justify-content-center items-center">
-                    ${ data.facebook ? `<a class="mx-2" href="${data.facebook}" target="_blank"><i class="fab fa-facebook"></i></a>` : `<a class="mx-2" href="/" target="_blank"><i class="fab fa-facebook"></i></a>`}
-                    ${ data.instagram ? `<a class="mx-2" href="${data.instagram}" target="_blank"><i class="fab fa-instagram"></i></a>` : `<a class="mx-2" href="/" target="_blank"><i class="fab fa-instagram"></i></a>`}
+                    ${data.facebook ? `<a class="mx-2" href="${data.facebook}" target="_blank"><i class="fab fa-facebook"></i></a>` : `<a class="mx-2" href="/" target="_blank"><i class="fab fa-facebook"></i></a>`}
+                    ${data.instagram ? `<a class="mx-2" href="${data.instagram}" target="_blank"><i class="fab fa-instagram"></i></a>` : `<a class="mx-2" href="/" target="_blank"><i class="fab fa-instagram"></i></a>`}
                     </div>
             </div>
         </div>
@@ -80,3 +80,93 @@ LEGENDS.map((data,index) =>{
     container.appendChild(card);
 })
 
+const MANIFESTO = [
+    {   
+        id: '5rg1weg52we1g3wer21',
+        title: 'General Facilities',
+        data: [
+            'Properly maintained AC common rooms with refurnished seating and indoor games in every hostel',
+            'Increased number of subsidised cafeterias',
+            'Pooling and justified Auto-rickshaw fares',
+            'Measures against snakes and mosquitoes',
+            'Proper treatment/relocation of injured campus dogs ',
+            'High Speed Internet facilities across campus',
+            'Restoration of Speed Breakers and Proper Barricading outside main gate',
+        ],
+    },
+    {
+        id: '3s5dg1werg231',
+        title: 'Academic Facilities',
+        data: [
+
+            '24*7 open library and increase in number of desks and plug points',
+            'Incubation centre for startups',
+            'Available interview rooms with high speed internet for online interviews',
+            'Free access to various softwares using institute credentials',
+            'Pre-placement/internship mock sessions and mentorship ',
+            'Mentorship for English and communication skill development',
+        ],
+    },
+    {
+        id: 'adgv13as2g1as3d',
+        title: 'Healthcare and Cultural Activities',
+        data: [
+            'Reopen ovals for 24hrs for better practice',
+            'Renovated gym with brand new equipments and extension of timings till 11 P.M.',
+            'Introduction of new fests and restoration of Food Fest',
+            'Medical store inside campus',
+            'Improved Health Insurance for all students ',
+            'Better Lighting and levelling of Ovals and Lords grounds and reconstruction of proper cricket pitches',
+            'Proper court flooring for tennis and basketball courts',
+            'Encouragement in Inter-NITS through sponsored and reimbursement of equipments',
+        ],
+    },
+    {
+        id: 'asd3eg561sa3gd1',
+        title: 'Essential Facilities',
+        data: [
+            'Mess system for all halls instead of company-tender system',
+            'Re-entry of online food delivery services through hostel gates ',
+            'Reopening of shopping complex',
+            'Vending machines for sanitary pads',
+            'Vending machines for snacks',
+            'Introduction of Night canteen',
+            'Laundry and stationary facilities (especially printing and photocopy) at every hall',
+            'Egg, Paneer and Dahi Pantry at Mess',
+            'Hygenic isolation of Veg and Non Veg food and utensils in Mess',
+        ],
+    },
+]
+
+
+MANIFESTO.map((data,index) => {
+    const card = document.createElement('div');
+    card.className = 'man col-12 col-lg-6 my-3'
+    card.innerHTML = `
+        <div class="card py-2">
+            <div class="card-body">
+            <h4 class="text-white head mb-3 text-center">${data.title}</h4>
+            <hr class="bg-white">
+            <div class="accordion" id="${data.id}">
+                    ${data.data.map((item, index) => {
+                        return `
+                        <div class="accordion-item mb-3">
+                            <h2 class="accordion-header my-0" id="flushhead-${data.id}-${index}">
+                                <button class="accordion-button collapsed real" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-${data.id}-${index}" aria-expanded="false" aria-controls="flush-${index}">
+                                    ${item}
+                                </button>
+                            </h2>
+                            <div id="flush-${data.id}-${index}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
+                                data-bs-parent="#${data.title}">
+                                <div class="accordion-body">Placeholder content for this accordion</div>
+                            </div>
+                        </div>
+                        `
+                    }).join('')}
+                </div>
+            </div>
+        </div>
+    `;
+    manifesto.appendChild(card);
+})
